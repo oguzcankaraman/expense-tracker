c5d63bf5f11c73b3af4e26f220f311b9c2b385fb
oguzcan <oguzcan07karaman@gmail.com>
2025-10-04 22:59:11 +0300

    configure applşcation.properties for railway

--- backend/src/main/java/com/expense/expense_tracker/config/WebConfig.java
+++ backend/src/main/java/com/expense/expense_tracker/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${allowed-origins:http://localhost:3000}")
+    @Value("${ALLOWED_ORIGINS:http://localhost:3000}")
     private String allowedOrigins;
 
     @Override
