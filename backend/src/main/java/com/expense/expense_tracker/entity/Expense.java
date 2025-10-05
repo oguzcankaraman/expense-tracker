b0b3900f6ae86c8b4a19c36bfd00d5b5266bcc2d
oguzcan <oguzcan07karaman@gmail.com>
2025-10-05 09:59:33 +0300

    feature: create User entity and configure database relationship with Expense

--- backend/src/main/java/com/expense/expense_tracker/entity/Expense.java
+++ backend/src/main/java/com/expense/expense_tracker/entity/Expense.java
@@ -1,10 +1,7 @@
 package com.expense.expense_tracker.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -24,4 +21,8 @@ public class Expense {
     private String category;
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
